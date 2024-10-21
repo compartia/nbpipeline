ba133339b227fbf252446c6e1cff2f498db5f329
Artem Zaborskiy <compartia@gmail.com>
2024-10-21 19:21:10 +0800

    Update run.py

--- nbpipeline/run.py
+++ nbpipeline/run.py
@@ -23,40 +23,91 @@ logging.info(f'Notebooks directory: {notebooks_dir}')
 logging.info(f'Reports directory: {REPORTS_DIR}')
 
 #############################################
-PIPELINE_STAGES = [  # (notebook_name, urls)
-    ('sample_stage_1', 'stage1'),
-    ('sample_stage_2', 'stage2'),    
-]
-#############################################
-
-stop_scheduler = threading.Event()
 
 #############################################
 
 
-def generate_navigation_html():
-    """Generate a simple HTML with a list of links for navigation based on PIPELINE_STAGES."""
-    links = [
-        f'<li><a href="/{url}">{notebook_name.replace("_", " ").title()}</a></li>'
-        for notebook_name, url in PIPELINE_STAGES if url
-    ]
 
-    scheduler_status = (
-        "<p>The scheduler is stopped, see error logs</p>"
-        if stop_scheduler.is_set()
-        else f"<p>The scheduler is running, next run scheduled to "
-             f"{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(schedule.next_run().timestamp()))}</p>"
-    )
+#############################################
 
-    html = (
-        "<html><body><h1>Logs</h1><ul>" +
-        "".join(links) +
-        "<h2>Scheduler Status</h2>" +
-        scheduler_status +
-        "</ul></body></html>"
-    )
+class NBPipeliner():
+    def __init__(self, stages=None):
+        """
+        Initialize the NBPipeliner with optional pipeline stages.
 
-    return html
+        :param stages: List of tuples containing notebook names and URLs. 
+                       Defaults to PIPELINE_STAGES if not provided.
+        """
+        self.stages = stages
+        logging.info(f'Initialized NBPipeliner with stages: {self.stages}')
+
+    def job(self):
+        """Job to execute notebooks and handle errors."""
+        for notebook_name, _ in self.stages:
+            if not exec_note(notebook_name):
+                self.stop_scheduler.set()
+                break
+    
+    def _init_routing(self):
+        self.app.add_url_rule('/', endpoint='home', view_func=self.generate_navigation_html)
+
+        for notebook_name, url in self.stages:
+            self.app.add_url_rule(f'/{url}', endpoint=notebook_name,
+                            view_func=lambda notebook_name=notebook_name: serve_stage_results_html(notebook_name))
+
+
+    def run_scheduler(self):
+        """Run the scheduled jobs."""
+        while not self.stop_scheduler.is_set():
+            schedule.run_pending()
+            time.sleep(1)
+
+    def start(self):
+        self.stop_scheduler = threading.Event()
+
+        interval_minutes = int(os.environ.get('NBP_DEFAULT_SCHEDULE_INTERVAL_MINUTES', 10))
+        schedule.every(interval_minutes).minutes.do(self.job)
+
+        scheduler_thread = threading.Thread(target=self.run_scheduler, daemon=True)
+        scheduler_thread.start()
+
+
+        self.run_flask()
+        
+
+    def run_flask(self):
+        """Main function to set up the scheduler and start the Flask app."""
+        self.app = Flask(__name__)
+    
+        port = int(os.environ.get('NPB_SERVER_PORT', 8088))
+        host = str(os.environ.get('NPB_SERVER_HOST', '0.0.0.0'))
+
+        self._init_routing()
+        self.app.run(host=host, port=port, use_reloader=False)
+
+    def generate_navigation_html(self):
+        """Generate a simple HTML with a list of links for navigation based on PIPELINE_STAGES."""
+        links = [
+            f'<li><a href="/{url}">{notebook_name.replace("_", " ").title()}</a></li>'
+            for notebook_name, url in self.stages if url
+        ]
+
+        scheduler_status = (
+            "<p>The scheduler is stopped, see error logs</p>"
+            if self.stop_scheduler.is_set()
+            else f"<p>The scheduler is running, next run scheduled to "
+                f"{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(schedule.next_run().timestamp()))}</p>"
+        )
+
+        html = (
+            "<html><body><h1>Logs</h1><ul>" +
+            "".join(links) +
+            "<h2>Scheduler Status</h2>" +
+            scheduler_status +
+            "</ul></body></html>"
+        )
+
+        return html
 
 
 def make_html(input_notebook):
@@ -108,51 +159,21 @@ def exec_note(script_name):
     return no_error
 
 
-def job():
-    """Job to execute notebooks and handle errors."""
-    for notebook_name, _ in PIPELINE_STAGES:
-        if not exec_note(notebook_name):
-            stop_scheduler.set()
-            break
-
-
-def run_scheduler():
-    """Run the scheduled jobs."""
-    while not stop_scheduler.is_set():
-        schedule.run_pending()
-        time.sleep(1)
-
 
 def serve_stage_results_html(html_name):
     return send_from_directory(REPORTS_DIR, f'{html_name}.html')
 
 
-def _init_routing(app):
-    app.add_url_rule('/', endpoint='home', view_func=generate_navigation_html)
-
-    for notebook_name, url in PIPELINE_STAGES:
-        app.add_url_rule(f'/{url}', endpoint=notebook_name,
-                         view_func=lambda notebook_name=notebook_name: serve_stage_results_html(notebook_name))
-
-
 def main():
-    """Main function to set up the scheduler and start the Flask app."""
-    app = Flask(__name__)
+    # job()
+    
 
-    interval_minutes = int(os.environ.get('NBP_DEFAULT_SCHEDULE_INTERVAL_MINUTES', 10))
-    schedule.every(interval_minutes).minutes.do(job)
-
-    scheduler_thread = threading.Thread(target=run_scheduler, daemon=True)
-    scheduler_thread.start()
-
-    port = int(os.environ.get('NPB_SERVER_PORT', 8088))
-    host = str(os.environ.get('NPB_SERVER_HOST', '0.0.0.0'))
-
-    _init_routing(app)
-
-    app.run(host=host, port=port, use_reloader=False)
-
+    __PIPELINE_STAGES = [  # (notebook_name, urls)
+        ('sample_stage_1', 'stage1'),
+        ('sample_stage_2', 'stage2'),    
+    ]
+    x = NBPipeliner(__PIPELINE_STAGES)
+    x.start()
 
 if __name__ == "__main__":
-    # job()
     main()
